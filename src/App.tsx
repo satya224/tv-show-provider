17ad5f4e06ad4e85dcf2fd10c5f13bdef0eb3ea8
satyaprakash <satyaprakash101010@gmail.com>
2023-05-31 09:20:01 +0530

    WIP - Fetch tv shows and display 1st tv show completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import styles from './App.module.css'
+import {TvShow} from "./api/tv-show";
+import {INITIAL_TV_SHOW} from "./api/initial-tv-show";
+import {BACKDROP_BASE_URL} from "./config";
+import TVShowDetail from "./components/TVShowDetail/TVShowDetail";
+import Logo from "./components/Logo/Logo";
+import logoImg from "./assets/images/logo.png"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [currentTVShow, setCurrentTVShow] = useState(INITIAL_TV_SHOW);
+
+    const fetchPopularTVShowList = async () => {
+        const popularTVShowList = await TvShow.fetchPopulars();
+        if (popularTVShowList.length > 0)
+            setCurrentTVShow(popularTVShowList[0]);
+    }
+    useEffect(() => {
+        fetchPopularTVShowList().then()
+    }, [])
+
+    return (
+        <div className={styles.App} style={{
+            background: currentTVShow
+                ? `linear-gradient(rgba(0, 0, 0, 0.55), rgba(0, 0, 0, 0.55)),
+             url("${BACKDROP_BASE_URL}${currentTVShow.backdrop_path}") no-repeat center / cover`
+                : "black",
+        }}>
+            <div className={styles.header}>
+                <div className={"row"}>
+                    <div className={"col-4"}>
+                        <Logo img={logoImg} title={"WatoWatch"} subtitle={"Find a show you may like"}/>
+                    </div>
+                    <div className={"col-md-12 col-lg-4"}>
+                        <input style={{width: "100%"}} type={"text"}/>
+                    </div>
+                </div>
+            </div>
+            <div className={styles.tv_show_detail}><TVShowDetail tvShow={currentTVShow}/></div>
+            <div className={styles.recommended_tv_shows}>Recommended TV shows</div>
+        </div>
+    );
 }
 
 export default App;
